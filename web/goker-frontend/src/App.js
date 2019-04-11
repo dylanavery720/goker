27dd5e228de95d58b079c4987bf9f7291a2473f9
Dylan A <dylanpatrickavery@gmail.com>
2019-04-10 20:28:08 -0600

    Setup selects and prepare hand to be sent to server

--- web/goker-frontend/src/App.js
+++ web/goker-frontend/src/App.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {getRank} from "./Actions/requests"
+import Select from "./Select/Select"
+import {deck, suits} from "./Constants/constants"
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      rank: null
+      rank: "",
+      card1: "",
+      card1Suit: "",
+      card2: "",
+      card2Suit: "",
+      card3: "",
+      card3Suit: "",
+      card4: "",
+      card4Suit: "",
+      card5: "",
+      card5Suit: ""
     }
   }
 
   componentDidMount() {
     //TODO: Use Picky for Dropdown to select 5 cards. Submit to get rank.
+  }
+
+  handleClick(value, identifier) {
+    this.setState({[identifier]: value })
+  }
+
+  submitCards() {
+    const {card1, card1Suit, card2, card2Suit, card3, card3Suit, card4, card4Suit, card5, card5Suit} = this.state
+    let hand = ""
+    hand = `${card1}${card1Suit} ${card2}${card2Suit} ${card3}${card3Suit} ${card4}${card4Suit} ${card5}${card5Suit}`
+    console.log(hand,'ranky')
   }
 
   render() {
@@ -32,6 +56,22 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <p>Card 1</p>
+        <Select handleClick={(value) => this.handleClick(value, "card1")} options={Object.keys(deck)}/>
+        <Select handleClick={(value) => this.handleClick(value, "card1Suit")} options={Object.keys(suits)}/>
+        <p>Card 2</p>
+        <Select handleClick={(value) => this.handleClick(value, "card2")} options={Object.keys(deck)}/>
+        <Select handleClick={(value) => this.handleClick(value, "card2Suit")} options={Object.keys(suits)}/>
+        <p>Card 3</p>
+        <Select handleClick={(value) => this.handleClick(value, "card3")} options={Object.keys(deck)}/>
+        <Select handleClick={(value) => this.handleClick(value, "card3Suit")} options={Object.keys(suits)}/>
+        <p>Card 4</p>
+        <Select handleClick={(value) => this.handleClick(value, "card4")} options={Object.keys(deck)}/>
+        <Select handleClick={(value) => this.handleClick(value, "card4Suit")} options={Object.keys(suits)}/>
+        <p>Card 5</p>
+        <Select handleClick={(value) => this.handleClick(value, "card5")} options={Object.keys(deck)}/>
+        <Select handleClick={(value) => this.handleClick(value, "card5Suit")} options={Object.keys(suits)}/>
+        <button onClick={() => this.submitCards()}></button>
       </div>
     );
   }
---
+++ web/goker-frontend/src/Constants/constants.js
@@ -0,0 +1,23 @@
+export const deck = {
+	"A": 14,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
+	"10": 10,
+	"J": 11,
+	"Q": 12,
+	"K": 13,
+}
+
+
+export const suits = {
+    "s": "spades",
+    "d": "diamonds",
+    "h": "hearts",
+    "c": "clubs",
+}
